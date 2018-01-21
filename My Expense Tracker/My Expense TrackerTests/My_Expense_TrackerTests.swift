99127cba9afd775a20c637bf312326ae6b0873f5
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2018-01-20 19:17:54 -0500

    Initial commit

---
+++ My Expense Tracker/My Expense TrackerTests/My_Expense_TrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  My_Expense_TrackerTests.swift
+//  My Expense TrackerTests
+//
+//  Created by Chetwyn on 1/20/18.
+//  Copyright © 2018 Clarke Enterprises. All rights reserved.
+//
+
+import XCTest
+@testable import My_Expense_Tracker
+
+class My_Expense_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
